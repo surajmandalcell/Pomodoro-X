01f9443b7258cf353e449e98f70b04e592421719
surajmandalcell <surajmandalcell@gmail.com>
2019-04-14 20:59:26 +0530

    checkpoint

--- Form1.h
+++ Form1.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 namespace PomodoroX {
 
@@ -34,6 +34,16 @@ namespace PomodoroX {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Label^ label1;
+	protected:
+
+
+	private: System::Windows::Forms::Button^ button3;
+
+
+	protected:
+
+	protected:
 
 	protected:
 
@@ -51,14 +61,48 @@ namespace PomodoroX {
 		void InitializeComponent(void)
 		{
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(Form1::typeid));
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->button3 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Arial", 27.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->ForeColor = System::Drawing::Color::White;
+			this->label1->Location = System::Drawing::Point(12, 9);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(191, 42);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Pomodoro";
+			// 
+			// button3
+			// 
+			this->button3->BackColor = System::Drawing::Color::IndianRed;
+			this->button3->FlatAppearance->BorderSize = 0;
+			this->button3->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button3->Font = (gcnew System::Drawing::Font(L"Inconsolata", 8.249999F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button3->ForeColor = System::Drawing::SystemColors::ControlLightLight;
+			this->button3->Location = System::Drawing::Point(331, -1);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(53, 30);
+			this->button3->TabIndex = 3;
+			this->button3->TabStop = false;
+			this->button3->Text = L"✖";
+			this->button3->UseVisualStyleBackColor = false;
+			this->button3->Click += gcnew System::EventHandler(this, &Form1::Button3_Click);
 			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(321, 411);
-			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedDialog;
+			this->BackColor = System::Drawing::Color::DarkSlateBlue;
+			this->ClientSize = System::Drawing::Size(383, 566);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->label1);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
 			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
 			this->MaximizeBox = false;
 			this->MinimizeBox = false;
@@ -66,10 +110,21 @@ namespace PomodoroX {
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"Pomodoro X";
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
 	private: System::Void TextBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
 	}
-	};
+	private: System::Void TextBox1_TextChanged_1(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void Button1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void Button2_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+private: System::Void Button3_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void Button4_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+};
 }
